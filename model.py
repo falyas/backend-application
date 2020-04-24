54259177b601ea2458f70c80d6c25bee191950b1
farahalyasar <farah.alyasar@gmail.com>
2020-04-24 07:46:12 -0700

    add item function

--- model.py
+++ model.py
@@ -1,4 +1,4 @@
-from flask import  Flask, render_template
+from flask import  Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 # create an application that gets named after the name of our file
@@ -24,17 +24,30 @@ class Todo(db.Model):
 # otherwise this call, does nothing
 db.create_all()
 
-# Set up a route to listen to our homepage, the route handler is index
-@app.route('/')
+# Set up a route endpoint to handle user form input
+@app.route('/todos/create', methods=['POST'])
+def create_todo():
+    todo_description = request.form.get('description', '')
+    if todo_description == '':
+        skip
+    else:
+        # add record to database
+        todo_item = Todo(description=todo_description)
+        db.session.add(todo_item)
+        db.session.commit()
+        # refresh records and redirect user to the index endpoint
+        return redirect(url_for('index'))
 
+# Set up a route endpoint to listen to our homepage, the route handler is index
+@app.route('/')
 # This method acts as the controller. It routes commands to models and views
 # return a template html file to render an
-# html file for the user whenever the user visits this route
+# html file for the user when222222ever the user visits this route
 def index():
     # by default, Flask looks for template files in a file called templates
     # inside the project directory, so index.html is inside the templates folder
     # Pass data to render_template with list of items to render in our todo list
-    return render_template('index.html', data=Todo.query.all())
+    return render_template('view.html', data=Todo.query.all())
 
 if __name__ == '__main__':
     app.run(debug=True)
